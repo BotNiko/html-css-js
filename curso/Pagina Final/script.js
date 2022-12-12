5e0862c95b5e49b376e4307a16c931a8977f9a3b
Nicolas <111761342+BotNiko@users.noreply.github.com>
2022-12-12 19:46:53 -0300

    add

--- curso/Pagina Final/script.js
+++ curso/Pagina Final/script.js
@@ -1,6 +1,7 @@
 if(prompt("Ingrese su Edad") >= 18){
     window.location.href = "pagina.html";
 }else{
-    alert("Usted esta accediendo a esta pagina bajo su responsabilidad");
-    window.location.href = "pagina.html";
+    if (confirm("Usted continua bajo su responsabilidad!")) {
+        window.location.href = "pagina.html";
+    }
 }
